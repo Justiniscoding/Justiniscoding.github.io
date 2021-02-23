d9168906d7634fa2d0cb5fdade42a1d9f5704353
Justiniscoding <76059890+Justiniscoding@users.noreply.github.com>
2021-02-22 20:05:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("Hello");
+function readFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+const tileset = readFile('./tileset');
+const tilecols = readFile('./tilecols');
+alert(tileset);
+alert(tilecols);
