8d0dc92aa66f96459cc15f72d60fad8ca46241b8
justiniscoding <nanxnan@proton.me>
2023-10-24 08:53:09 +0200

    fix bug where css and js were no loaded in ghpages

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+        appDir:"app"
 	}
 };
 
