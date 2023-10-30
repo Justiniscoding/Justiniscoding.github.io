95742e0d7e803837de381f69b1f006084ef92635
justiniscoding <nanxnan@proton.me>
2023-10-30 10:07:31 -0400

    update hover animation and make icons links

--- src/components/socials.svelte
+++ src/components/socials.svelte
@@ -1,9 +1,19 @@
 <div class="socials">
-    <Fa class=icon scale=1x icon={faEnvelope} color=white/>
-    <Fa class=icon icon={faDiscord} color=white/>
-    <Fa class=icon icon={faGithub} color=white/>
-    <Fa class=icon icon={faStackOverflow} color=white/>
-    <Fa class=icon icon={faCodepen} color=white/>
+    <a href="mailto:nanxnan@proton.me">
+        <Fa class=icon scale=1x icon={faEnvelope} color=white/>
+    </a>
+    <a href="https://discordapp.com/users/953044340631011379">
+        <Fa class=icon icon={faDiscord} color=white/>
+    </a>
+    <a href="https://github.com/justiniscoding">
+        <Fa class=icon icon={faGithub} color=white/>
+    </a>
+    <a href="https://stackoverflow.com/users/14940054/justiniscoding">
+        <Fa class=icon icon={faStackOverflow} color=white/>
+    </a>
+    <a href="https://codepen.io/Justiniscoding-the-looper">
+        <Fa class=icon icon={faCodepen} color=white/>
+    </a>
 </div>
 
 <script>
@@ -20,15 +30,35 @@
         gsap.utils.toArray(".icon").forEach((icon,i) => {
             var tween = gsap.to(icon.querySelectorAll("path"),{
                 paused:true,
-                fill:"#5865F2",
+                fill:getIconColorFromIndex(i),
                 scale:1.5,
-                transformOrigin:"center"
+                transformOrigin:"center",
+                duration:0.25
             });
 
-            icon.addEventListener("mouseenter", () => tween.play());
-            icon.addEventListener("mouseleave", () => tween.reverse());
+            icon.addEventListener("mouseenter", () => {
+                tween.timeScale(1);
+                tween.play();
+            });
+            icon.addEventListener("mouseleave", () => {
+                tween.timeScale(0.5);
+                tween.reverse();
+            });
         });
     });
+
+    function getIconColorFromIndex(index){
+        switch (index) {
+            case 0:
+                return "#39ff14"
+            case 1:
+                return "#5865F2"
+            case 3:
+                return "#FF9900"
+            default:
+                return "white"
+        }
+    }
 
 </script>
 
